0970d01cc1e000b8d2e08c43596143ccb04d6edd
amanbh321 <Aman.Bahuguna@iiitb.ac.in>
2026-01-30 09:34:29 +0000

    Add Subtraction Functionality

--- src/calc.cpp
+++ src/calc.cpp
@@ -5,9 +5,22 @@ int add(int a, int b) {
     return a + b;
 }
 
+int subtract(int a, int b) {
+    return a - b;
+}
+
 int main() {
     int a, b;
-    cin >> a >> b;
-    cout << add(a, b) << endl;
+    char op;
+    cin >> a >> op >> b;
+
+    if (op == '+') {
+        cout << add(a, b) << endl;
+    } else if (op == '-') {
+        cout << subtract(a, b) << endl;
+    } else {
+        cout << "Invalid operator" << endl;
+    }
+
     return 0;
 }
